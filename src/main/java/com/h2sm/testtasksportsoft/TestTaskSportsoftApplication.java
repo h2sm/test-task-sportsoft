43b5693e98aa08b10024d06f1142107bb50e5f19
Egor Fedorenko <fedore.egor@gmail.com>
2023-03-11 14:26:05 +0300

    Add .gitignore and other files

---
+++ src/main/java/com/h2sm/testtasksportsoft/TestTaskSportsoftApplication.java
@@ -0,0 +1,13 @@
+package com.h2sm.testtasksportsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskSportsoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskSportsoftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/h2sm/testtasksportsoft/TestTaskSportsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h2sm.testtasksportsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskSportsoftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
